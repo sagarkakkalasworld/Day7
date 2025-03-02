e198798db89dc4de5ae1a0c166b8056130274123
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-02 08:12:43 +0000

    Update deploy.sh

--- Bash/deploy.sh
+++ Bash/deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-microk8s kubectl delete ns react-microk8s  ###deleting namespace is never recommended
-microk8s kubectl create ns react-microk8s
 microk8s kubectl apply -f /home/ubuntu/bashscripts/Day7/Kubernetes/
