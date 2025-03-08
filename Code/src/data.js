221c95fc3117eb32d02468aedac0f8c7e09f58b3
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-08 08:45:01 +0000

    Update data.js

--- Code/src/data.js
+++ Code/src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "SSL has been configured successfully",
+    title: "SSL has been configured successfully for our Domain",
     desc: "This is now in new commit",
     bg: "green",
   },
